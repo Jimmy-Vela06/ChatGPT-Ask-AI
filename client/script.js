8228d6e0d799703487991badb6c1e156b38a4d66
Jimmy Vela <jimmyvela06@.com>
2023-02-19 15:11:43 -0600

    deploy

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
   loadingResponse(messageDiv);
 
-  const response = await fetch("http://localhost:3000", {
+  const response = await fetch("https://ask-ai-rslv.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
